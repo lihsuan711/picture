7f6785d178cb104788e472b325a96efa83c7970a
lihsuan <l22761712@gmail.com>
2021-05-23 01:07:51 +0800

    third version

--- picture.py
+++ picture.py
@@ -3,8 +3,9 @@ import os
 
 
 
-for picture in os.listdir('.'):
+for picture in os.listdir('orig'):
     if picture.endswith('.jpg'):
-        img = Image.open(picture)   # 读取图片
+        img = Image.open(os.path.join('orig',picture))   # 读取图片
+        # 也可以寫成img = Image.open('orig/' + picture)
         img = img.convert("L")   # 转化为黑白图片
-        img.save(picture[:-4] + "_grey.jpg")   # 存储图片
+        img.save(os.path.join('result',picture[:-4] + '_grey.jpg'))   # 存储图片
