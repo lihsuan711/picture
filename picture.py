c4b6aa07a1888f977b3097462a80a8be3cffd096
lihsuan <l22761712@gmail.com>
2021-05-22 22:52:12 +0800

    second version

--- picture.py
+++ picture.py
@@ -1,5 +1,10 @@
 from PIL import Image
+import os
 
-img = Image.open("tomato.jpg")   # 读取图片
-img = img.convert("L")   # 转化为黑白图片
-img.save("444.jpg")   # 存储图片
+
+
+for picture in os.listdir('.'):
+    if picture.endswith('.jpg'):
+        img = Image.open(picture)   # 读取图片
+        img = img.convert("L")   # 转化为黑白图片
+        img.save(picture[:-4] + "_grey.jpg")   # 存储图片
